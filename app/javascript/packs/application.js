7b405e7a0370810ca96ac19580d094b13b81e9a9
Shirley Alvarado <shirley@codecrc.com>
2021-04-09 14:11:23 -0600

    ChartKick installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,62 +16,70 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-jQuery(function() {
-    console.log('==============Ya está instalado jQuery, y funciona!==============')
-    console.log(jQuery)
-    console.log($)
-})
+// jQuery(function() {
+//     console.log('Ya esta instalado jQuery, y funciona!!')
+//     console.log(jQuery)
+//     console.log($)
+// })
+// console.log($(document.body))
 
-console.log($(document.body))
 
-$(document).on('turbolinks:load', function() { //todas las acciones que modifiquen el Dom deben estar incluidas en esta function
-    //$('body').css('background', 'tomato')
-    //$('body').css('color', 'white')
-    $('body').css({ background: 'tomato', color: 'white' })
-    const colors = ['tomato', 'navy', 'mediumseagreen', 'orange', 'skyblue', 'chucknorris']
+// $(document).on('turbolinks:load', function() {
+//     /*$('body').css('background', 'tomato')
+//     $('body').css('color', 'white')*/
+//     $('body').css({ background: 'tomato', color: 'white' })
+//     const colors = ['tomato', 'navy', 'mediumseagreen', 'orange', 'skyblue', 'crimson', 'chucknorris']
 
-    $('.alert').click(function() {
-            alert('ALERTA, me han hecho click!')
-        })
-        //document.querySelector('.title') //Selecciona al 1ero que encuentre
-    $('select').change(function() {
-        $('.result').text($(this).val())
-    })
-    $('h1').hover(function() {
-        console.log('me rosaste, me siento ofendido')
-    })
-    $('input.nombre').focus(function() {
-        console.log('recuerde escribir su nombre, NO el apellido!')
-    })
-    $('input.nombre').blur(function() {
-        const txt = $(this).val()
-        if (txt) {
-            console.log('Bien, escribiste tu nombre!')
-        } else {
-            console.log('Ya pues, y el nombre?')
-        }
-    })
-    $('.hide').click(function() {
-        $('.text').hide('slow', function() {
-            $('body').css('background', 'navy')
-        })
-    })
-    $('.show').click(function() {
-        $('.text').show(5000, function() {
-            $('body').css('background', 'tomato')
-        })
-    })
-    $('.toggle').click(function() {
-        $('.text').toggle(500, function() {
-            $('body').css('background', colors[Math.floor(Math.random() * colors.length)])
-        })
-    })
+//     $('.alert').click(function() {
+//         alert('ALERTA, me han hecho click!')
+//     })
+//     $('select').change(function() {
+//         $('.result').text($(this).val())
+//     })
+//     $('h1').hover(function() {
+//         console.log('Me rosaste!!, Me siento ofendido!!')
+//     })
+//     $('input.nombre').focus(function() {
+//         console.log('recuerde de poner su nombre, no me venga a poner el apellido!')
+//     })
+//     $('input.nombre').blur(function() {
+//         const txt = $(this).val()
+//         if (txt) {
+//             $('.text span').prepend(`<i>${$(this).val()}: </i>`)
+//             console.log('Bien escribiste tu nombre!')
+//         } else {
+//             console.log('Ya pues, y el nombre?')
+//         }
+//     })
+//     $('input.nombre').on('keyup', function() {
+//         $('.result').html("<strong>" + $(this).val() + "</strong>")
+//     })
+//     $('.hide').click(function() {
+//         $('.text').hide('slow', function() {
+//             $('body').css('background', 'navy')
+//         })
+//     })
+//     $('.show').click(function() {
+//         $('.text').show(5000, function() {
+//             $('body').css('background', 'tomato')
+//         })
+//     })
+//     $('.toggle').click(function() {
+//         $('.text').toggle(500, function() {
+//             $('body').css('background', colors[Math.floor(Math.random() * colors.length)])
+//         })
+//     })
+//     $('.fadeout').on('click hover', function() {
+//         $('.himage').fadeOut().css('width', '10px')
+//     })
+//     $('.fadein').on('click hover', function() {
+//         $('.himage').fadeIn().css('width', '500px')
+//     })
+//     $('.fadetoggle').on('click', function() {
+//         $('.himage').fadeToggle()
+//     })
 
-    $('.fadeout').on('click hover', function() {
-        $('.himage').fadeOut()
-    })
-
-    $('.fadein').on('click hover', function() {
-        $('.himage').fadeIn()
-    })
-})
+//     colors.forEach(function(color) {
+//         $('.colors').append(`<li>${color}</li>`)
+//     })
+// })
